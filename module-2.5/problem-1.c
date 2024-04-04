b90a1a14c20b5eca6b1d0e4c507d06e67fda94f8
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-04-05 01:15:01 +0600

    assignment

---
+++ module-2.5/problem-1.c
@@ -0,0 +1,11 @@
+/* You need to print the lines given below in the same format as it is -
+
+ “Recently I heard that you’ve achieved 95% marks in your exam. 
+ This is brilliant! 
+ I wish you’ll shine in your life!	Good luck with all the barriers(/\) in your life.”*/
+
+#include<stdio.h>
+int main(){
+    printf("Recently I heard that you’ve achieved 95%% marks in your exam.\nThis is brilliant!\n I wish you’ll shine in your life!\tGood luck with all the barriers(/\\) in your life\n.");
+    return 0;
+}

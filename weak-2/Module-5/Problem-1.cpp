d1a18a89b68567a327906b0691b794fbbaa3e68e
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-04-07 12:55:57 +0600

    some changes

---
+++ weak-2/Module-5/Problem-1.cpp
@@ -0,0 +1,18 @@
+// I. Welcome for you with Conditions
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    if (A > B || A == B)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+    return 0;
+}

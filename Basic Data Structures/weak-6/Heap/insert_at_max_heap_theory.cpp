3d595de2d5c1f5976832ee392b2bdb7874ece912
“abdurrahim220” <z.abdurrahim5@gmail.com>
2024-10-03 11:08:35 +0600

    heap complete

---
+++ Basic Data Structures/weak-6/Heap/insert_at_max_heap_theory.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // vector<int> v = {50, 40, 45, 30, 35, 42, 32, 25, 20, 10};
+    vector<int> v;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+
+        int x;
+        cin >> x;
+        v.push_back(x);
+        int cur_inx = v.size() - 1;
+        // cout<<cur_inx<<endl;
+
+        while (cur_inx != 0)
+        {
+            int parent_idx = (cur_inx - 1) / 2;
+            if (v[parent_idx] < v[cur_inx])
+            {
+                swap(v[parent_idx], v[cur_inx]);
+            }
+            else
+            {
+                break;
+            }
+            cur_inx = parent_idx;
+        }
+    }
+    for (int val : v)
+    {
+        cout << val << " ";
+    }
+    return 0;
+}

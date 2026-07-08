f4bcd29f5729c909c464059124b476759dad0d68
Md AbdurRahim <79690351+abdurrahim220@users.noreply.github.com>
2026-07-08 06:55:22 +0000

    module 1 added

---
+++ Basic Data Structures/weak-1/module-1/0_N.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+   int ar[n];
+    for (int i = 0; i < n; i++) {    // O(n)
+        cin >> ar[i];
+    }
+
+    int s = 0;
+    for (int i = 0; i < n; i++) {  // O(n)
+        s += ar[i];
+    }
+
+    cout << s << endl;
+    return 0;
+}

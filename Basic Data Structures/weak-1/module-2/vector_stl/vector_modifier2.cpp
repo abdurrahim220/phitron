d4243090254af3cdbcd76aeab5dac34da2b2e226
abdurrahim220 <z.abdurrahim5@gmail.com>
2026-08-06 17:55:57 +0600

    basic c++

--- Basic Data Structures/weak-1/module-2/vector_stl/vector_modifier2.cpp
+++ Basic Data Structures/weak-1/module-2/vector_stl/vector_modifier2.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 int main()
 {
-    vector<int> x = {1, 2, 2, 3, 4, 5,2};
+    vector<int> x = {1, 2, 2, 3, 6, 8, 7 3, 4, 5, 2};
 
-    replace(x.begin(), x.end()-1, 2, 20);
-    for(int v:x){
-        
-        cout<<v<<" ";
-    }
+    // replace(x.begin(), x.end()-1, 2, 20);
+    // for(int v:x){
+
+    //     cout<<v<<" ";
+    // }
+
+    cout << find(x.begin(), x.end(), 2) << endl;
+
     return 0;
 }

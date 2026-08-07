7c2d87be2592cc7a12ed8dfe881ae0d7fabc8c55
abdurrahim220 <z.abdurrahim5@gmail.com>
2026-08-07 17:51:50 +0600

    basic_c++ comp

---
+++ Introduction to C++ for DSA/weak-1-again/basic-C++/array_string.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char s[100];
+    int a;
+    cin >> a;
+
+    // cin >> s;
+
+    getchar();
+
+    fgets(s, 100, stdin);
+    // cout << strlen(s) << endl;
+    cout<< a << endl;
+    cout << s << endl;
+
+    return 0;
+}
---
+++ Introduction to C++ for DSA/weak-1-again/basic-C++/min_max_swap.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// void swap(int &a, int &b)
+// {
+//     int temp = a;
+//     a = b;
+//     b = temp;
+// }
+int main()
+{
+
+    int a, b;
+    cin >> a >> b;
+    swap(a, b);
+    cout << "After swapping: " << a << " " << b << endl;
+    // int a, b, c, d;
+    // cin >> a >> b >> c >> d;
+
+    // int c = min(a, b);
+    // int d = max(a, b);
+    // int mn= min({a, b, c, d});
+    // int mx= max({a, b, c, d});
+    // cout << "Minimum: " << c << endl;
+    // cout << "Maximum: " << d << endl;
+    // cout << "Minimum: " << mn << endl;
+    // cout << "Maximum: " << mx << endl;
+
+    return 0;
+}
